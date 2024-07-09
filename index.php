ab1ed64446739a08128231307ff1ba1c5c659bd7
vdcfvhbxzvhjbs <121741836+vdcfvhbxzvhjbs@users.noreply.github.com>
2024-07-09 17:48:30 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+simple access counter for php3
+(c)1998 David W. Bettis
+dbettis@eyeintegrated.com
+medify by Wilson Peng
+*/
+$counterFile = "/tmp/counter.txt";
+function displayCounter($counterFile) {
+$fp = fopen($counterFile,"rw");
+$num = fgets($fp,5);
+$num += 1;
+print "$num";
+exec( "rm -rf $counterFile");
+exec( "echo $num > $counterFile");
+}
+
+if (!file_exists($counterFile)) {
+exec( "echo 0 > $counterFile");
+}
+
+displayCounter($counterFile);
+
+?>
